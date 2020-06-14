683ad07f76ec7f169c9e6d3927971b7723d09cde
ernestina404 <ernestinaspinu@gmail.com>
2020-06-14 12:01:49 +0000

    Added a form validation script from https://mdbootstrap.com/docs/jquery/forms/validation/. Prevent submission of the contact form is the fields are not filled in or are invalid.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Back to Top Button Script
 //Get the button:
 mybutton = document.getElementById("myBtn");
 
@@ -17,3 +18,32 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+
+
+//Form Validation Script 
+// MB Bootstrap - source > https://mdbootstrap.com/docs/jquery/forms/validation/
+(function() {
+'use strict';
+window.addEventListener('load', function() {
+// Fetch all the forms we want to apply custom Bootstrap validation styles to
+var forms = document.getElementsByClassName('needs-validation');
+var elmnt = document.getElementById("contact");
+// Loop over them and prevent submission
+var validation = Array.prototype.filter.call(forms, function(form) {
+form.addEventListener('submit', function(event) {
+if (form.checkValidity() === false) {
+event.preventDefault();
+event.stopPropagation();
+}
+form.classList.add('was-validated');
+
+//Removes the form after succesful submission
+if (form.checkValidity() === true) {
+  var  elmnt = document.getElementById("form_group");
+  elmnt.scrollIntoView();
+elmnt.style.display = 'none';
+}
+
+}, false);});}, false);
+})();
